909a1319e3823b98185d1f77a1fe26597232f6a4
Leonel Castañeda <leodcastaneda@gmail.com>
2018-12-26 12:19:51 -0600

    add utils file

--- client/js/master.js
+++ client/js/master.js
@@ -1,32 +1,4 @@
-function getUrlParameter(sParam) {
-  var sPageURL = window.location.search.substring(1),
-  sURLVariables = sPageURL.split('&'),
-  sParameterName,
-  i;
-
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
-
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-    }
-  }
-};
-
-
 $(function(){
-  if (!String.prototype.format) {
-    String.prototype.format = function() {
-      var args = arguments;
-      return this.replace(/{(\d+)}/g, function(match, number) {
-        return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-      });
-    };
-  }
-
   var mapConfig = {
     "initial_view": [24.59, -103.14],
     "initial_zoom": 5,
---
+++ client/js/utils.js
@@ -0,0 +1,27 @@
+// define "str{0}-{1}".format("a","b")
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+      ;
+    });
+  };
+}
+
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+  sURLVariables = sPageURL.split('&'),
+  sParameterName,
+  i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+};
